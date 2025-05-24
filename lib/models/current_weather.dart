0ba1e9a94e067a0f2cfe96ce9ff89ded2e490ad6
NicolasAConstantin <156631305+NicolasAConstantin@users.noreply.github.com>
2025-05-23 18:57:32 -0600

    finished project
    Fixed issues with dart sdk
    
    Added late
    
    Created weather_api

--- lib/models/current_weather.dart
+++ lib/models/current_weather.dart
@@ -1,10 +1,10 @@
 class CurrentWeather {
-  String _city;
-  String _description;
-  double _currentTemp;
-  DateTime _currentTime;
-  DateTime _sunrise;
-  DateTime _sunset;
+  late String _city;
+  late String _description;
+  late double _currentTemp;
+  late DateTime _currentTime;
+  late DateTime _sunrise;
+  late DateTime _sunset;
 
   String get city => _city;
   String get description => _description;
@@ -28,6 +28,18 @@ class CurrentWeather {
     _sunrise = sunrise;
     _sunset = sunset;
   }
+
+  factory CurrentWeather.fromOpenWeatherData(dynamic data) {
+    return CurrentWeather(
+      city: data['name'],
+      description: data['weather'][0]['description'],
+      currentTemp: data['main']['temp'],
+      currentTime: DateTime.fromMillisecondsSinceEpoch((data['dt'] * 1000).toInt()),
+      sunrise: DateTime.fromMillisecondsSinceEpoch((data['sys']['sunrise'] * 1000).toInt()),
+      sunset: DateTime.fromMillisecondsSinceEpoch((data['sys']['sunset'] * 1000).toInt()),
+    );
+  }
+
 
   set city(String value) {
     if (value.trim().isEmpty) {
@@ -56,10 +68,10 @@ class CurrentWeather {
         value.year != _currentTime.year) {
       throw Exception('Sunrise must be on the same day as current time');
     }
-
     if (value.isAfter(_sunset)) {
       throw Exception('Sunrise cannot be after sunset');
     }
+
     _sunrise = value;
   }
 
@@ -82,4 +94,11 @@ class CurrentWeather {
     }
     _description = value;
   }
+
+  @override
+  String toString() {
+    return 'City: $_city, Description: $_description, Current Temperature: $_currentTemp, Current Time: $_currentTime, Sunrise: $_sunrise, Sunset: $_sunset';
+  }
 }
+
+
--- lib/services/weather_api.dart
+++ lib/services/weather_api.dart
@@ -1,1 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
+const String weatherApiKey = '69e54fdcad371ca9a1a64acef3e0a654';
+const String currentWeatherEndpoint = 'https://api.openweathermap.org/data/2.5/weather';
+
+Future<dynamic> getWeatherForCity({required String city}) async {
+  final url = Uri.parse('$currentWeatherEndpoint?units=metric&q=$city&appid=$weatherApiKey');
+
+  try {
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('There was a problem with the request: status ${response.statusCode} received.');
+    }
+  } catch (e) {
+    throw Exception('There was a problem with the request: $e');
+  }
+}
